191cda723d78cef86140ca7f0a6e5d0071b2cff6
Hussein66253 <hussein66253@icloud.com>
2022-08-25 13:32:14 +0300

    register and login routes added also dotenv sample added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,22 +1,42 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
 
 const JWTPRIVATEKEY = process.env.JWTPRIVATEKEY;
 
 const userSchema = new mongoose.Schema({
-  Name: { type: String, required: true },
+  name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   country: { type: String, required: true },
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, JWTPRIVATEKEY, {
+  console.log(this.name,this._id);
+  const token = jwt.sign({ _id: this._id, name: this.name }, JWTPRIVATEKEY, {
     expiresIn: "7d",
   });
   return token;
 };
 
 const User = mongoose.model("user", userSchema);
+const validateRegister = (registerData) => {
+  const schema = Joi.object({
+    name: Joi.string().required().label("name"),
+    email: Joi.string().email().required().label("email"),
+    password: passwordComplexity().required().label("Password"),
+    country: Joi.string().required().label("country"),
+  });
+  return schema.validate(registerData);
+};
 
-module.exports = { User };
+const validateLogin = (loginData) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().label("email"),
+    password: Joi.string().required().label("password"),
+  });
+  return schema.validate(loginData);
+};
+
+module.exports = { User, validateRegister,validateLogin };
